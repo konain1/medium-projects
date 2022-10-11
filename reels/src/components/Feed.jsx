93b9327195184489d97f10c97ab2cc2872ee3242
konain1 <neeruali99@gmail.com>
2022-10-11 22:58:23 +0530

    feed page design

--- reels/src/components/Feed.jsx
+++ reels/src/components/Feed.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import './Feed.css'
 
 function Feed() {
   return (
-    <div>
-      <h1>feed</h1>
+    <>
+    <div className='main__Feed_cont'>
+     <div className='header' > 
+
+     <div className='insta_logo'>
+     <img className='logo_img' src='https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png'/>
+
+     </div>
+
+     <div className='profile_pic'>
+      <img className='pic_img' src='https://images.unsplash.com/photo-1665427803235-8e295131ad29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'/>
+     </div>
+
+     </div>
+
+     <div className='upload'>
+     <div className='upload_div'>
+     <h3>upload</h3>
+     <i class="fa-solid fa-film mymovie-icon"></i> 
+     </div>
+    
+     </div>
+
+
     </div>
+    </>
   )
 }
 
